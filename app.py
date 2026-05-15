b35a5009c27108a274ff76095d7820f5173be259
Devapriya Anil <devapriyaanil373@gmail.com>
2026-05-15 12:30:53 +0530

    update ap

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import streamlit as st
 import pickle
+import re
+
 model = pickle.load(open("model.pkl", "rb"))
-st.title("CA2 Sentiment Analysis")
-review = st.text_area("Enter Movie Review")
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"<.*?>", "", text)
+    text = re.sub(r"[^a-zA-Z]", " ", text)
+    return text
+
+st.title("Movie Review Sentiment Analysis")
+
+review = st.text_area("Enter your review")
+
 if st.button("Predict"):
-    prediction = model.predict([review])
-    st.success(f"Sentiment : {prediction[0]}")
+    cleaned_review = clean_text(review)
+    prediction = model.predict([cleaned_review])[0]
+
+    if prediction == "positive":
+        st.success("Positive Review ")
+    else:
+        st.error("Negative Review ")
